cc310d041a399015532cd54db54bf6ecffcbbfe0
Adam Tate <lincolnluxor@gmail.com>
2014-10-13 17:11:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,207 @@
+#CRATE.JS
+
+A modal creation library with no external depedencies.
+
+##Why use crate.js?
+
+There are a ton of modal libraries out there, each with their own pros and cons. Here are a few reasons to use Crate.js:
+
+###Simple initialization
+
+1. Set up your options object (we also made a handy tool for this)
+2. Download and install the source
+3. Call ```crate.init(options)```
+4. ...Really, that's it.
+
+###Lightweight
+
+Don't worry about page load speed. Extremely tiny in size (1.4KB gzipped or 4.89KB minified or 8.35KB with no compression).
+
+###Flexibility
+
+Crate.js uses many options that are included in the popular modal creating libraries.
+
+- Want to close the modal with a key press? Crate.js can do it.
+- Want to set the overlay to a specific color? Go for it.
+- Need a function to be called after your crate is closed? Standard feature.
+- Read the "Option Parameters" below for a full list of available options.
+
+###Control
+
+You have the option to control crate using crate.js' multitude of options. From setting the overlay color, to controlling when to close the crate so you can fire some animations at the perfect time.
+
+#INITIALIZATION:
+Create a javascript object with your requested setting parameters.
+
+Call
+```crate.init(options);```
+
+##OPTION PARAMETERS:
+
+
+###*setBody [REQUIRED]
+- pass in full html markup as a string
+- has a default class of crate-body
+- Example: ```{'setBody':'Some String'}``` or ```{'setBody':'<div class="styled">Some HTML</div>'}```
+
+###setTitle
+- pass in full html markup as a string
+- has a default class of crate-title
+- Example: ```{'setBody':'Some String', 'setTitle':'Some Title'}```
+
+###setFooter
+- pass in full html markup as a string
+- has a default class of crate-footer
+- Example: ```{'setBody':'Some String', 'setFooter':'Some Footer'}```
+
+###setParent
+- pass a string of the element you want to house the crate container
+- defaults to body
+- Example: ```{'setBody':'Some String', 'setParent':'.more'}```
+
+###setPosition
+- pass an array of the top style and left style in px
+- any css measurement can be passed in as a string
+- defaults to center
+- Example: ```{'setBody':'Some String', 'setPosition':'['calc(25% - 200px','14em']'}```
+
+###closeActions
+- pass in an object with params below
+  - button
+    - pass in true or false for a button to appear in the title
+    - has a default class of crate-close
+    - defaults to true
+    - if no title is set and button is on, a blank title with a button will appear
+    - Example: ```{'setBody':'Some String', 'closeActions':{'button':true}```
+
+  - clickIn
+    - pass in true or false for the crate to close when you click inside of the container
+    - Example: ```{'setBody':'Some String', 'closeActions':{'clickIn':true}```
+
+  - clickOut
+    - pass in true or false for the crate to close when you click outside of the container
+    - Example: ```{'setBody':'Some String', 'closeActions':{'clickOut':true}```
+
+  - pressKeys
+    - pass in an array of numbers assigned to keyCodes that will close the crate
+    - use javascript keyCode numbers only
+    - Example: ```{'setBody':'Some String', 'closeActions':{'pressKeys':[27]}``` (p.s. 27 is the esc key)
+
+  - Any combination
+    - Example: ```{'setBody':'Some String', 'closeActions':{'button':true,'clickOut':true,'pressKeys':[27]}```
+
+  - Leave it blank for NO WAY to close the crate besides calling the public function to do so
+  - Example: ```{'setBody':'Some String', 'closeActions':{}```
+
+###setOverlay
+- pass in a string of a hex color, rgb, or rgba for the container to be colored
+- container has a default class of crate-contain
+- defaults to rgba(0,0,0,.8)
+- Example: ```{'setBody':'Some String', 'setOverlay':'#ff0000'}```
+
+###attachClass
+- pass in an object of selectors (eg classes (.class) or ids (#id)) and styles to be added to the selector
+- good for adding animations to the container of the crate
+- Single Use Example: ```{'setBody':'Some String', 'attachClass':{'.crate-container':'some-animation'}}```
+- Multi Use Example: ```{'setBody':'Some String', 'attachClass':{'.crate-container':'some-animation','.crate-footer':'something-else'}}```
+
+###onOpen
+####option 1:
+- Pass in a number to be used as a delay in ms before the crate is opened
+- Good if you are running an animation on init and want to delay the opening of the crate
+- Example: ```{'setBody':'Some String', 'onOpen':250}```
+
+####option 2:
+- Pass in a function to be used as the crate is opened
+- Good if you want to execute more javascript after the crate has been opened
+- Example: ```{'setBody':'Some String', 'onOpen':function open() {alert('open');}}```
+
+####option 3:
+- Pass in an object made of a delay parameter with a number in ms and an action parameter with a function
+- This will execute the function after the delay on open.
+- Mixture of option 1 and 2
+- Example: ```{'setBody':'Some String', 'onOpen':{'delay':250,'action':function open() {alert('open');}}}```
+
+###onClosing
+- same options as onOpen, except executed when the crate is closed
+- Example: ```{'setBody':'Some String', 'onClosing':{'delay':250,'action':function close() {alert('close');}}}```
+
+##EXAMPLE OBJECTS
+
+###Completely filled out
+```
+{'setBody':'<div>Some HTML</div>','setParent':'.more','setPosition':[0,0],'setTitle':'<div>Some Title</div>','setFooter':'<div>Some HTML</div>','closeActions':{'button': true,'clickIn': false,'clickOut': false,'pressKeys': [27],},'setOverlay':'rgba(255,255,255,.5)','attachClass':{'.crate-body':'blue'},'onOpen':{'delay':1000,'action':function open() {alert('open');}},'onClosing':{'delay':1000,'action':function close() {alert('close');}}};
+```
+
+###Bare minimum
+```
+{'setBody':'Some string'};
+```
+
+#USEFUL PUBLIC FUNCTIONS:
+You have availability to run these functions at any time after crate.init has ran
+
+###crate.addClass(object)
+- pass in the same object you would as in the init with the attachClass param
+
+###crate.destroyCrate()
+- will delete the crate and also run the function and delay if you passed it in the onClosing init param
+
+###crate.openCrate()
+- will show the crate and also run the function and delay if you passed it in the onOnen init param
+
+###crate.showCrate()
+- will show the crate
+
+###crate.hideCrate()
+- will hide the crate
+
+###crate.toggleCrate()
+- will toggle the display of the crate
+
+#NEW CSS CLASSES
+```.crate-contain```
+
+```.crate-content```
+
+```.crate-title``` (child of ```.crate-content```) [if applicable]
+
+```.crate-close``` (child of ```.crate-title```) [if applicable]
+
+```.crate-body``` (child of ```.crate-content```)
+
+```.crate-footer``` (child of ```.crate-content```) [if applicable]
+
+#COMPATIBILITY
+
+Crate.js has been tested with:
+
+#####Windows
+- Chrome 38
+- Firefox 32
+- IE 10
+- Safari 5 - There is a bug with the default centering since crate.js uses css translate to exact center the element
+
+#####Mac
+- Chrome 38
+- Firefox 32
+- Safari 8 - There is a bug with the default centering since crate.js uses css translate to exact center the element
+
+#####Ubuntu
+- Chrome 37
+- Firefox 32
+
+#####Android
+- Chrome 38
+- Firefox 32
+
+#####iOS
+- Safari
+
+#VERSION HISTORY
+
+v0.1 - 10/13/2014 - Initial release
+
+#CRATE MAKER
+
+Want an easier way to create your crate.js options, use the [crate maker tool](http://cratemaker.html)
---
+++ crate.js
@@ -0,0 +1,226 @@
+var crate = (function() {
+  var api = {};
+  api.init = function(ob) {
+    if (api.isSet(ob.setBody)) {//setBody is REQUIRED!
+      api.destroyCrates(); //One modal at a time ladies
+      function setActions(src, target) {
+        if(typeof src == 'object') {
+          api[target + 'Delay'] = (typeof src.delay == 'number') ? src.delay : false;
+          api[target + 'Action'] = (typeof src['action'] == 'function') ? src['action'] : false;
+        } else {
+          api[target + 'Delay'] = (typeof src == 'number') ? src : false;
+          api[target + 'Action'] = (typeof src == 'function') ? src : false;
+        }
+      };
+      setActions(ob.onOpen, 'crateOpening');
+      setActions(ob.onClosing, 'crateClosing');
+      api.createCrate(ob);
+    } else {
+      console.error("crate.js: setBody is required in the object passed to the init function.");
+      return false;
+    }
+  };
+
+  api.isSet = function(item) {
+    //Check to see if the properties are set in the object
+    if (typeof item === "undefined" || item === "" || item === null) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  api.createCrate = function(ob) {
+    //Private functions of the init function
+    function createElement(type, className, location) {
+      //Creating the new elements
+      var el = document.createElement(type);
+      el.className = className;
+      location.appendChild(el);
+      return el;
+    };
+    function addListener(el,action,result) {
+      //Add listeners
+      if (action === 'click') {
+        if (el.addEventListener) {
+          el.addEventListener(action, result);
+        } else if (el.attachEvent) { //IE8
+          el.attachEvent('on' + action, result);
+        };
+      } else if (action === 'keyup') {
+        if (el.addEventListener) {
+          el.addEventListener(action, result, false); //27 is the escape button
+        } else if (el.attachEvent) { //IE8
+          el.attachEvent(action, result, false);
+        };
+      };
+    };
+    //Read the object and assign variables
+    api.crateParent = (api.isSet(ob.setParent)) ? document.querySelector(ob.setParent) : document.body;
+    api.isPositioned = api.isSet(ob.setPosition);
+    api.crateTop = (api.isPositioned) ? ob.setPosition[0] : null;
+    api.crateLeft = (api.isPositioned) ? ob.setPosition[1] : null;
+    api.crateTitle = (api.isSet(ob.setTitle)) ? ob.setTitle : null;
+    api.crateBody = ob.setBody;
+    api.crateFooter = (api.isSet(ob.setFooter)) ? ob.setFooter : null;
+    if (api.isSet(ob.closeActions)) {
+      if (api.isSet(ob.closeActions) && !api.isSet(ob.closeActions.button)) {
+        api.crateButton = false;
+      } else {
+        api.crateButton = (api.isSet(ob.closeActions.button)) ? ob.closeActions.button : true;
+      }
+      api.crateClickIn = (api.isSet(ob.closeActions.clickIn)) ? ob.closeActions.clickIn : false;
+      api.crateClickOut = (api.isSet(ob.closeActions.clickOut)) ? ob.closeActions.clickOut : false;
+      api.cratePressKeys = (api.isSet(ob.closeActions.pressKeys)) ? ob.closeActions.pressKeys : false;
+    } else {
+      api.crateButton = true;
+    }
+    api.crateOverlay = (api.isSet(ob.setOverlay)) ? ob.setOverlay : 'rgba(0,0,0,.8)';
+    api.crateClasses = (api.isSet(ob.attachClass)) ? ob.attachClass : false;
+
+    //Create the container
+    api.crateContainer = createElement('div','crate-contain',api.crateParent);
+    api.styleElement(api.crateContainer, {'display':'none','top':'0','left':'0','right':'0','bottom':'0','z-index':'500','background':api.crateOverlay});
+
+    //Create the content
+    api.crateContent = createElement('div','crate-content',api.crateParent);
+    if (api.isPositioned) {
+      if (api.isSet(ob.setPosition[0]) && api.isSet(ob.setPosition[1])) {//top and left both defined
+        api.styleElement(api.crateContent,{'display':'none','z-index':'501','top':api.crateTop,'left':api.crateLeft});
+      } else if (api.isSet(ob.setPosition[0]) && !api.isSet(ob.setPosition[1])) {//top defined, no left
+        api.styleElement(api.crateContent,{'display':'none','z-index':'501','top':api.crateTop,'left':'50%','transform':'translate(-50%, 0)'});
+      } else if (!api.isSet(ob.setPosition[0]) && api.isSet(ob.setPosition[1])) {//no top, left defined
+        api.styleElement(api.crateContent,{'display':'none','z-index':'501','top':'50%','left':api.crateLeft,'transform':'translate(0, -50%)'});
+      }
+    } else {//no top, no left
+      api.styleElement(api.crateContent,{'display':'none','z-index':'501','top':'50%','left':'50%','transform':'translate(-50%, -50%)'});
+    }
+
+    //Center of the body or center of an element?
+    if (api.isSet(ob.setParent)) {
+      api.styleElement(api.crateContainer,{'position':'absolute'});
+      api.styleElement(api.crateContent,{'position':'absolute'});
+    } else {
+      api.styleElement(api.crateContainer,{'position':'fixed'});
+      api.styleElement(api.crateContent,{'position':'fixed'});
+    }
+
+    //Create the header
+    if (api.isSet(ob.setTitle)) {
+      api.crateTitle = createElement('div','crate-title',api.crateContent);
+      api.crateTitle.innerHTML = ob.setTitle;
+    } else if (!api.isSet(ob.setTitle) && api.crateButton) {
+      api.crateTitle = createElement('div','crate-title',api.crateContent);
+    }
+
+    //Create the body
+    if (api.isSet(ob.setBody)) {
+      api.crateBody = createElement('div','crate-body',api.crateContent);
+      api.crateBody.innerHTML = ob.setBody;
+    }
+
+    //Create the footer
+    if (api.isSet(ob.setFooter)) {
+      api.crateFooter = createElement('div','crate-footer',api.crateContent);
+      api.crateFooter.innerHTML = ob.setFooter;
+    }
+
+    //Create the close and bind actions
+    if (api.isSet(ob.closeActions)) {
+      if (api.crateButton) {
+        api.crateClose = createElement('div','crate-close',api.crateTitle);
+        addListener(api.crateClose, 'click', api.destroyCrates);
+      }
+      if (api.crateClickIn) {
+        addListener(api.crateContent, 'click', api.destroyCrates);
+      }
+      if (api.crateClickOut) {
+        addListener(api.crateContainer, 'click', api.destroyCrates);
+      }
+      if (api.cratePressKeys) {
+        addListener(window, 'keyup', function(e) {
+          for (var i = 0; i < api.cratePressKeys.length; i++) {
+            if (e.keyCode == api.cratePressKeys[i]) {
+              api.destroyCrates();
+              break;
+            }
+          }
+        });
+
+      }
+    } else { //default to a button
+      api.crateClose = createElement('div','crate-close',api.crateTitle);
+      addListener(api.crateClose, 'click', api.destroyCrates);
+    }
+
+    if (api.crateClasses) {
+      api.addClasses(api.crateClasses);
+    }
+    api.openCrate();
+  };
+
+  api.styleElement = function(el, styles) {
+    for (e in styles) {el.style[e] = String(styles[e]);}
+  };
+
+  api.addClasses = function(classes) {
+    for (e in classes) {
+      el = document.querySelector(e);
+      el.className = el.className + " " + classes[e];
+    }
+  };
+
+  api.destroyCrates = function() {
+    if (typeof api.crateContainer !== 'undefined' && typeof api.crateContent !== 'undefined') {
+      function closeAction() {
+        api.crateContainer.parentNode.removeChild(api.crateContainer);
+        api.crateContent.parentNode.removeChild(api.crateContent);
+        //Reset the variables to be able to open the crate more than once
+        delete api.crateContainer;
+        delete api.crateContents;
+        if (api.isSet(api.crateClosingAction) && typeof api.crateClosingAction == 'function') {
+          api.crateClosingAction();
+        }
+      }
+      if (api.isSet(api.crateClosingDelay)) {
+        setTimeout(closeAction,api.crateClosingDelay);
+      } else {
+        closeAction();
+      }
+    }
+
+  };
+
+  api.openCrate = function() {
+    function openAction() {
+      if (api.isSet(api.crateOpeningAction) && typeof api.crateOpeningAction == 'function') {
+        api.crateOpeningAction();
+      }
+      api.showCrate();
+    }
+    if (api.isSet(api.crateOpeningDelay)) {
+      setTimeout(openAction,api.crateOpeningDelay);
+    } else {
+      openAction();
+    }
+  };
+
+  api.showCrate = function() {
+    api.styleElement(api.crateContainer,{'display':'block'});
+    api.styleElement(api.crateContent,{'display':'block'});
+  };
+
+  api.hideCrate = function() {
+    api.styleElement(api.crateContainer,{'display':'none'});
+    api.styleElement(api.crateContent,{'display':'none'});
+  };
+
+  api.toggleCrate = function() {
+    if (api.crateContainer.style.display === 'none') {
+      api.showCrate();
+    } else {
+      api.hideCrate();
+    }
+  };
+  return api;
+})();
